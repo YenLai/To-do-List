7c2ff3a5090be72ecf523259cf573119cff413f8
Laisam <b540915@gmail.com>
2020-05-15 14:56:23 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -16,20 +16,6 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 
-// 取得 mongoose.connect() 連上的默認 Connection object
-const db = mongoose.connection
-
-//db.on => register a listener to monitor if there is error 
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-//db.once => a listener which can be only triggered once
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
 app.listen(3000, () => {
   console.log('app is running on http://localhost:3000.')
 })
